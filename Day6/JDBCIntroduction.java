1fbd3c3445a63637b6fa9006c58aff883b08ce8c
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-06-02 16:01:14 +0530

    Create JDBCIntroduction.java

---
+++ Day6/JDBCIntroduction.java
@@ -0,0 +1,19 @@
+
+/*
+JDBC Introduction
+- JDBC = Java Database Connectivity
+- Used to connect Java applications with databases
+- Steps:
+  1. Import JDBC packages
+  2. Load driver (optional in modern JDBC)
+  3. Create Connection
+  4. Create Statement/PreparedStatement
+  5. Execute SQL
+  6. Process ResultSet
+  7. Close resources
+*/
+public class JDBCIntroduction {
+    public static void main(String[] args) {
+        System.out.println("JDBC Basics");
+    }
+}
