033e8796eaf13c66c6488db925023cc9c8f17df1
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-06-02 20:48:16 +0530

    Create ServletConfigAndContext class with documentation
    Add ServletConfigAndContext class with comments explaining ServletConfig and ServletContext.

---
+++ Day7/ServletConfigAndContext.java
@@ -0,0 +1,17 @@
+
+import jakarta.servlet.*;
+
+public class ServletConfigAndContext {
+
+    /*
+    ServletConfig:
+    - Per servlet configuration
+
+    ServletContext:
+    - Shared by entire application
+
+    Methods:
+    getInitParameter()
+    getServletContext()
+    */
+}
