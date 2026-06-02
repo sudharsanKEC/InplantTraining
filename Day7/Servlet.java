e5c0f99a6af1d589605c2d42a43ab17fb475b285
Sudharsan <155707028+sudharsanKEC@users.noreply.github.com>
2026-06-02 16:07:52 +0530

    Create Servlet.java

---
+++ Day7/Servlet.java
@@ -0,0 +1,25 @@
+
+/*
+Servlet Introduction
+
+Servlet = Java class that runs on a web server.
+Used to handle HTTP requests and responses.
+
+Lifecycle:
+1. init()
+2. service()
+3. destroy()
+
+Common Containers:
+- Apache Tomcat
+- Jetty
+
+Flow:
+Browser -> Servlet Container -> Servlet -> Response
+*/
+
+public class ServletIntroduction {
+    public static void main(String[] args) {
+        System.out.println("Learn Servlets");
+    }
+}
